8a440aa2e214b00dd9a216406d4b53cc0245c29d
Eileen <eileen@fuzion.co.nz>
2013-11-19 23:18:01 +1300

    Convert to module

--- flo2cashdonate.php
+++ flo2cashdonate.php
@@ -30,11 +30,80 @@
  * @copyright Fuzion Aotearoa Ltd (c) 2013
  */
 
-require_once 'CRM/Core/Payment.php';
 
+
+require_once 'flo2cashdonate.civix.php';
+
+/**
+ * Implementation of hook_civicrm_config
+ */
+function flo2cashdonate_civicrm_config(&$config) {
+  _flo2cashdonate_civix_civicrm_config($config);
+}
+
+/**
+ * Implementation of hook_civicrm_xmlMenu
+ *
+ * @param $files array(string)
+ */
+function flo2cashdonate_civicrm_xmlMenu(&$files) {
+  _flo2cashdonate_civix_civicrm_xmlMenu($files);
+}
+
+/**
+ * Implementation of hook_civicrm_install
+ */
+function flo2cashdonate_civicrm_install() {
+  return _flo2cashdonate_civix_civicrm_install();
+}
+
+/**
+ * Implementation of hook_civicrm_uninstall
+ */
+function flo2cashdonate_civicrm_uninstall() {
+  return _flo2cashdonate_civix_civicrm_uninstall();
+}
+
+/**
+ * Implementation of hook_civicrm_enable
+ */
+function flo2cashdonate_civicrm_enable() {
+  return _flo2cashdonate_civix_civicrm_enable();
+}
+
+/**
+ * Implementation of hook_civicrm_disable
+ */
+function flo2cashdonate_civicrm_disable() {
+  return _flo2cashdonate_civix_civicrm_disable();
+}
+
+/**
+ * Implementation of hook_civicrm_upgrade
+ *
+ * @param $op string, the type of operation being performed; 'check' or 'enqueue'
+ * @param $queue CRM_Queue_Queue, (for 'enqueue') the modifiable list of pending up upgrade tasks
+ *
+ * @return mixed  based on op. for 'check', returns array(boolean) (TRUE if upgrades are pending)
+ *                for 'enqueue', returns void
+ */
+function flo2cashdonate_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
+  return _flo2cashdonate_civix_civicrm_upgrade($op, $queue);
+}
+
+/**
+ * Implementation of hook_civicrm_managed
+ *
+ * Generate a list of entities to create/deactivate/delete when this module
+ * is installed, disabled, uninstalled.
+ */
+function flo2cashdonate_civicrm_managed(&$entities) {
+  return _flo2cashdonate_civix_civicrm_managed($entities);
+}
 /**
  * Payment class for Flo2Cash Donate.
  */
+
 class nz_co_fuzion_flo2cashdonate extends CRM_Core_Payment {
 
   /**
