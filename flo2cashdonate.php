73349a4b386eb34212582a9758beb09f16738b34
Matt A <kiwibrogrammer@gmail.com>
2023-07-11 16:14:12 +1200

    civix upgrade remove php warnings

--- flo2cashdonate.php
+++ flo2cashdonate.php
@@ -12,15 +12,6 @@ function flo2cashdonate_civicrm_config(&$config) {
 }
 
 /**
- * Implements hook_civicrm_xmlMenu().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
- */
-function flo2cashdonate_civicrm_xmlMenu(&$files) {
-  _flo2cashdonate_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implements hook_civicrm_install().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
@@ -74,54 +65,6 @@ function flo2cashdonate_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
   return _flo2cashdonate_civix_civicrm_upgrade($op, $queue);
 }
 
-/**
- * Implements hook_civicrm_managed().
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
- */
-function flo2cashdonate_civicrm_managed(&$entities) {
-  _flo2cashdonate_civix_civicrm_managed($entities);
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types.
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function flo2cashdonate_civicrm_caseTypes(&$caseTypes) {
-  _flo2cashdonate_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Generate a list of Angular modules.
- *
- * Note: This hook only runs in CiviCRM 4.5+. It may
- * use features only available in v4.6+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_angularModules
- */
-function flo2cashdonate_civicrm_angularModules(&$angularModules) {
-  _flo2cashdonate_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
- */
-function flo2cashdonate_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _flo2cashdonate_civix_civicrm_alterSettingsFolders($metaDataFolders);
-}
-
 // --- Functions below this ship commented out. Uncomment as required. ---
 
 /**
@@ -129,9 +72,8 @@ function flo2cashdonate_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_preProcess
  *
-function flo2cashdonate_civicrm_preProcess($formName, &$form) {
 
-} // */
+ // */
 
 /**
  * Implements hook_civicrm_navigationMenu().
@@ -149,3 +91,12 @@ function flo2cashdonate_civicrm_navigationMenu(&$menu) {
   ));
   _flo2cashdonate_civix_navigationMenu($menu);
 } // */
+
+/**
+ * Implements hook_civicrm_entityTypes().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
+ */
+function flo2cashdonate_civicrm_entityTypes(&$entityTypes) {
+  _flo2cashdonate_civix_civicrm_entityTypes($entityTypes);
+}
